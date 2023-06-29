757e3bf0900c087379529560b9ca77fc6555763b
pachecopedroav <136128342+pachecopedroav@users.noreply.github.com>
2023-06-29 01:53:30 -0400

    Update ejercicio8.py

--- Python/ejercicio8/ejercicio8.py
+++ Python/ejercicio8/ejercicio8.py
@@ -1,1 +1,6 @@
+import calculadora
 
+print("4 + 2 es", calculadora.suma(4,2))
+print("4 - 2 es", calculadora.resta(4,2))
+print("4 * 2 es", calculadora.multiplica(4,2))
+print("4 / 2 es", calculadora.divide(4,2))
